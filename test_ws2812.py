d0121187dbc779bc84f37085ae76fc70edc6c736
Tom Soulanille <soul@prama.com>
2015-08-29 23:21:21 -0700

    ws2812: Make pixels accessable by attribute or index

--- test_ws2812.py
+++ test_ws2812.py
@@ -4,7 +4,10 @@ import unittest
 import logging
 
 import pyb
+import gc
 import math
+
+import random
 
 from ws2812 import WS2812
 
@@ -44,6 +47,7 @@ class WS2812TestCase(unittest.TestCase):
 
     def setUp(self):
         logging.basicConfig(level=logging.INFO)
+        random.seed("ws2812")
 
     def tearDown(self):
         pass
@@ -59,19 +63,35 @@ class WS2812TestCase(unittest.TestCase):
                          '11|11|11|11|11|11|11|11|11|11|11|11|0')
 
         # leds can be accessed via iterator
-        self.assertEqual(list(v for v in leds), [bytearray(3)])
+        self.assertEqual(list(list(v) for v in leds), [[0]*3])
 
         # Individual leds can be accessed by indexing
         pix = leds[0]
 
-        # Accessor provides a mutable indexable
-        pix[0] = 255
-        pix[1] = 255
-        pix[2] = 255
+        # pixels have r,g,b
+        pix.r = 1
+        pix.g = 2
+        pix.b = 3
+        self.assertEqual([pix.r, pix.g, pix.b], [1,2,3])
 
-        # leds can be mutated by indexing (only a pixel, not colors directly)
-        leds[0] = pix
-        self.assertEqual(list(v for v in leds), [bytearray((255,255,255))])
+        # pixels can also be indexed into for colors
+        self.assertEqual(list(pix), [1,2,3])
+        for i in range(len(pix)):
+            pix[i] = 12 * (i + 1)
+        self.assertEqual(list(pix), [12,24,36])
+
+        # A pixel position in a chain can be mutated by setting it with a bytearray
+        leds[0] = bytearray((7,11,92))
+        self.assertEqual(list(leds[0]), [7, 11, 92])
+        self.assertEqual([pix.r, pix.g, pix.b], [7, 11, 92])
+
+        # A pixel position in a chain can be mutated by setting it with bytes
+        leds[0] = b'foo'
+        self.assertEqual(list(leds[0]), [102, 111, 111])
+
+        # All-ones is represented correctly in the buffer
+        leds[0] = b'\xff\xff\xff'
+        self.assertEqual(list(leds[0]), [255, 255, 255])
         self.assertEqual('|'.join('%x' % v for v in leds.buf),
                          '33|33|33|33|33|33|33|33|33|33|33|33|0')
 
@@ -79,47 +99,56 @@ class WS2812TestCase(unittest.TestCase):
         pix[0] = 2
         pix[1] = 1
         pix[2] = 4
-        leds[0] = pix
         self.assertEqual('|'.join('%x' % v for v in leds.buf),
                          '11|11|11|13|11|11|11|31|11|11|13|11|0')
         # variation
         pix[0] = 12
         pix[1] = 34
         pix[2] = 56
-        leds[0] = pix
-        self.assertEqual(list(v for v in leds), [bytearray((12, 34, 56))])
+        self.assertEqual(list(leds[0]), [12, 34, 56])
         self.assertEqual('|'.join('%x' % v for v in leds.buf),
                          '11|31|11|31|11|11|33|11|11|33|31|11|0')
         # variation
         pix[0] = -1
         pix[1] = 345
         pix[2] = 777777777
-        leds[0] = pix
-        self.assertEqual(list(v for v in leds), [bytearray((255, 89, 113))])
+        self.assertEqual(list(leds[0]), [255, 89, 113])
         self.assertEqual('|'.join('%x' % v for v in leds.buf),
                          '13|13|31|13|33|33|33|33|13|33|11|13|0')
-
 
     def testGrindSinglePixel(self):
         # get / set work as expected
         leds = WS2812(spi_bus=1, led_count=1, intensity=1)
-        pix = bytearray(3)
         for i in range(1000):
-            for j in range(3):
-                pix[j] = pyb.rng()
-            leds[0] = pix
-            readback = leds[0]
-            self.assertEqual(readback, pix)
+            r = leds[0].r = random.getrandbits(8)
+            g = leds[0].g = random.getrandbits(8)
+            b = leds[0].b = random.getrandbits(8)
+            self.assertEqual(list(leds[0]), [r, g, b])
+
 
     def testMultiPixel(self):
         # buf is the correct length
+        # WS2812 can be iterated over to yield pixels
+        # pixel values can be set and read back
         for n in range(1, 400, 19):
+            leds = None
+            gc.collect()
             leds = WS2812(spi_bus=1, led_count=n)
-            self.assertEqual(len(leds.buf), 12*n + 1)
             self.assertEqual(len(leds), n)
-            zb = bytearray(3)
+            self.assertEqual(len(leds.buf), 12*n + 1)
+
+            random.seed(n)
             for pix in leds:
-                self.assertEqual(pix, zb)
+                self.assertEqual(list(pix), [0]*3)
+                for j in range(len(pix)):
+                    pix[j] = random.getrandbits(8)
+
+            pb = [0] * 3
+            random.seed(n)
+            for pix in leds:
+                for j in range(len(pix)):
+                    pb[j] = random.getrandbits(8)
+                self.assertEqual(list(pix), pb)
 
 
 def main():
--- ws2812.py
+++ ws2812.py
@@ -5,6 +5,56 @@ from array import array
 from uctypes import addressof, bytearray_at
 
 
+class Pixel:
+    def __init__(self, a, i):
+        self.a = a
+        self.i = i
+
+    @property
+    def r(self):
+        return _get(self.a, self.i+1)
+
+    @r.setter
+    def r(self, v):
+        _set(self.a, self.i+1, v)
+
+    @property
+    def g(self):
+        return _get(self.a, self.i)
+
+    @g.setter
+    def g(self, v):
+        _set(self.a, self.i, v)
+
+    @property
+    def b(self):
+        return _get(self.a, self.i+2)
+
+    @b.setter
+    def b(self, v):
+        _set(self.a, self.i+2, v)
+
+    def __getitem__(self, i):
+        if i >= 3 or i < 0:
+            raise IndexError("only 3 colors")
+        return _get(self.a, self.i + (1,0,2)[i])
+
+    def __setitem__(self, i, v):
+        if i >= 3 or i < 0:
+            raise IndexError("only 3 colors")
+        return _set(self.a, self.i + (1,0,2)[i], v)
+
+    def off(self):
+        self.r = self.b = self.g = 0
+
+    def __len__(self):
+        return 3
+
+    def __repr__(self):
+        return "<Pixel %d (%d, %d, %d) of chain 0x%x>" % \
+            (self.i//3, self.r, self.g, self.b, addressof(self.a))
+
+
 class WS2812:
     # Driver for WS2812 RGB LEDs. May be used for controlling single LED or chain
     # of LEDs.
@@ -41,6 +91,9 @@ class WS2812:
         self.buf = bytearray_at(addressof(self.a), 3*4*led_count + 1) # extra byte
         self.buf[-1] = 0        # make it \x00 to idle SPI low after transfer
 
+        # Prepare an empty cache by index of Pixel objects
+        self.pixels = [None] * led_count
+
         # OBSOLETE
         self.bits = array('L', range(256))
         bb = bytearray_at(addressof(self.bits), 4*256)
@@ -62,11 +115,11 @@ class WS2812:
     def __len__(self):
         return self.led_count
 
-    def get_led(self, index, rgb=None):
+    def get_led_values(self, index, rgb=None):
         # The asm function is unguarded as to index, so enforce here:
         if index >= self.led_count or index < 0:
             raise IndexError("tried to get item", index)
-        a_get = self.a_get
+        a_get = _get
         ix = index * 3
         rv = rgb or bytearray(3)
         rv[0] = a_get(self.buf, ix+1) # ws2812 part is green first
@@ -74,65 +127,35 @@ class WS2812:
         rv[2] = a_get(self.buf, ix+2) # then blue
         return rv
 
-    __getitem__ = get_led
+    def get_led_pixel(self, index):
+        # The asm function is unguarded as to index, so enforce here:
+        if index >= self.led_count or index < 0:
+            raise IndexError("tried to get item", index)
+        pixels = self.pixels
+        if pixels[index]:
+            return pixels[index]
+        pix = pixels[index] = Pixel(self.buf, index*3)
+        return pix
 
-    @staticmethod
-    @micropython.asm_thumb
-    def a_get(r0, r1):
-        # Registers:
-        # r0: base of array 'self.a' of 32-bit words
-        # r1: index into array
-        # r7: 4 * index (byte offset)
-        # r6: address of word in array
-        # r3: temporary
-        # r4: literals
-        # Note this could be improved by using the full Thumb instruction set. See:
-        # http://docs.micropython.org/en/latest/reference/asm_thumb2_hints_tips.html#use-of-unsupported-instructions
-        add(r7, r1, r1)     # * 2
-        add(r7, r7, r7)     # * 4
-        add(r6, r0, r7)     # base + 4 * index
-
-        # Notation: numeral indicates bit position in decoded result
-        # That bit may be True or False. Our job is to herd them to r0
-        # "-" is a clear bit (aka False or 0), "+" is a set bit (aka True or 1)
-        ldr(r3, [r6, 0])    # r3 is --1+--0+--3+--2+--5+--4+--7+--6+
-        mov(r0, r3)         # r0 is --1+--0+--3+--2+--5+--4+--7+--6+
-        mov(r4, 1)          # r4 is 1
-        lsr(r0, r4)         # r0 is ---1+--0+--3+--2+--5+--4+--7+--6
-        and_(r0, r3)        # r0 is ---1---0---3---2---5---4---7---6
-        mov(r3, r0)         # r3 is ---1---0---3---2---5---4---7---6
-        mov(r4, 3)          # r4 is 3
-        lsl(r0, r4)         # r0 is 1---0---3---2---5---4---7---6---
-        orr(r0, r3)         # r0 is 1--10--03--32--25--54--47--76--6
-        mov(r3, r0)         # r3 is 1--10--03--32--25--54--47--76--6
-        mov(r4, 10)
-        lsl(r0, r4)         # r0 is -32--25--54--47--76--6----------
-        orr(r0, r3)         # r0 is 1321025035432472576546-47--76--6
-        mov(r4, 7)
-        lsr(r0, r4)         # r0 is -------1321025035432472576546-47
-        movwt(r4, 0xf000f0) # r4 is --------++++------------++++----
-        and_(r0, r4)        # r0 is --------3210------------7654----
-        mov(r3, r0)         # r3 is --------3210------------7654----
-        mov(r4, 20)
-        lsr(r0, r4)         # r0 is ----------------------------3210
-        orr(r0, r3)         # r0 is --------3210------------76543210
-        mov(r4, 0xff)
-        and_(r0, r4)        # r0 is ------------------------76543210
-
+    __getitem__ = get_led_pixel
 
     def set_led(self, index, value):
         # set LED buffer at index to value
-        # value is bytearray((r,g,b))
+        # value is bytearray((r,g,b)) or bytes((r,g,b))
         # The asm function is unguarded as to index, so enforce here
         if index >= self.led_count or index < 0:
             raise IndexError
-        return self.a_set(self.buf, index, value)
+        if isinstance(value, bytes):
+            value = bytearray_at(addressof(value), 3)
+        elif isinstance(value, Pixel):
+            value = bytearray(list(value))
+        return self.a_set_rgb_values(self.buf, index, value)
 
     __setitem__ = set_led
 
     @staticmethod
     @micropython.asm_thumb
-    def a_set(r0, r1, r2):
+    def a_set_rgb_values(r0, r1, r2):
         # Register arguments:
         # r0: base of encoded pixel buffer (12 bytes / pixel)
         # r1: pixel #
@@ -275,3 +298,119 @@ class WS2812:
         a = self.a
         for i in range(end * 3, self.led_count * 3):
             a[i] = 0x11111111   # off
+
+
+@micropython.asm_thumb
+def _get(r0, r1):
+    # Registers:
+    # r0: base of array 'self.a' of 32-bit words
+    # r1: index into array
+    # r7: 4 * index (byte offset)
+    # r6: address of word in array
+    # r3: temporary
+    # r4: literals
+    # Note this could be improved by using the full Thumb instruction set. See:
+    # http://docs.micropython.org/en/latest/reference/asm_thumb2_hints_tips.html#use-of-unsupported-instructions
+    add(r7, r1, r1)     # * 2
+    add(r7, r7, r7)     # * 4
+    add(r6, r0, r7)     # base + 4 * index
+
+    # Notation: numeral indicates bit position in decoded result
+    # That bit may be True or False. Our job is to herd them to r0
+    # "-" is a clear bit (aka False or 0), "+" is a set bit (aka True or 1)
+    ldr(r3, [r6, 0])    # r3 is --1+--0+--3+--2+--5+--4+--7+--6+
+    mov(r0, r3)         # r0 is --1+--0+--3+--2+--5+--4+--7+--6+
+    mov(r4, 1)          # r4 is 1
+    lsr(r0, r4)         # r0 is ---1+--0+--3+--2+--5+--4+--7+--6
+    and_(r0, r3)        # r0 is ---1---0---3---2---5---4---7---6
+    mov(r3, r0)         # r3 is ---1---0---3---2---5---4---7---6
+    mov(r4, 3)          # r4 is 3
+    lsl(r0, r4)         # r0 is 1---0---3---2---5---4---7---6---
+    orr(r0, r3)         # r0 is 1--10--03--32--25--54--47--76--6
+    mov(r3, r0)         # r3 is 1--10--03--32--25--54--47--76--6
+    mov(r4, 10)
+    lsl(r0, r4)         # r0 is -32--25--54--47--76--6----------
+    orr(r0, r3)         # r0 is 1321025035432472576546-47--76--6
+    mov(r4, 7)
+    lsr(r0, r4)         # r0 is -------1321025035432472576546-47
+    movwt(r4, 0xf000f0) # r4 is --------++++------------++++----
+    and_(r0, r4)        # r0 is --------3210------------7654----
+    mov(r3, r0)         # r3 is --------3210------------7654----
+    mov(r4, 20)
+    lsr(r0, r4)         # r0 is ----------------------------3210
+    orr(r0, r3)         # r0 is --------3210------------76543210
+    mov(r4, 0xff)
+    and_(r0, r4)        # r0 is ------------------------76543210
+
+
+@micropython.asm_thumb
+def _set(r0, r1, r2):
+    # Register arguments:
+    # r0: base of encoded pixel buffer (12 bytes / pixel)
+    # r1: pixel offset e.g. 7 for red value of 3rd pixel in chain
+    # r2: value to set
+
+    # r3: address of encoded pixel
+    # r4: 2
+    # r5: base of data table
+    # r6: 3
+    # r7: temporary
+
+    mov(r5, pc)        # know the base of the data table
+    b(START)           # get to entry point
+    data(1, 0x11, 0x13, 0x31, 0x33) # encoded bytes corresponding to 2-bit values
+    align(2)           # ritual requirement
+
+    label(ENCODE)      # The encode(r1) entry point
+    # r1 is value in 0-255 to encode
+    # returns encoded word in r0
+    mov(r7, r1)        # r7 is value
+    and_(r7, r6)       # r7 is bottom two bits of value
+    add(r7, r7, r5)    # r7 is address of encoded data byte
+    ldrb(r0, [r7,0])   # r0 is encoded data byte
+
+    lsr(r1, r4)        # r1 is value >> 2
+    mov(r7, r1)        # r7 is value >> 2
+    and_(r7, r6)       # r7 is b3b2 of value
+    add(r7, r7, r5)    # r7 is address of encoded data byte
+    ldrb(r7, [r7,0])   # r7 is encoded data byte
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r4)        # r0 <<= 2 (in sum, r0 <<= 8)
+    orr(r0, r7)        # r0 half done
+
+    lsr(r1, r4)        # r1 is value >> 4
+    mov(r7, r1)        # r7 is value >> 4
+    and_(r7, r6)       # r7 is b5b4 of value
+    add(r7, r7, r5)    # r7 is address of encoded data byte
+    ldrb(r7, [r7,0])   # r7 is encoded data byte
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r4)        # r0 <<= 2 (in sum, r0 <<= 8)
+    orr(r0, r7)        # r0 three-quarters done
+
+    lsr(r1, r4)        # r1 is value >> 6
+    mov(r7, r1)        # r7 is value >> 6
+    and_(r7, r6)       # r7 is b7b6 of value
+    add(r7, r7, r5)    # r7 is address of encoded data byte
+    ldrb(r7, [r7,0])   # r7 is encoded data byte
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r6)        # r0 <<= 3
+    lsl(r0, r4)        # r0 <<= 2 (in sum, r0 <<= 8)
+    orr(r0, r7)        # r0 all done
+    bx(lr)
+
+    label(START)       # entry point
+
+    # Find the address of where we store result
+    add(r1, r1, r1)    # * 2
+    add(r1, r1, r1)    # * 4 = word width
+    add(r3, r0, r1)    # r3 is address of encoded 32-bit word
+
+    # set up useful constants
+    mov(r4, 2)
+    mov(r6, 3)
+
+    mov(r1, r2)        # pass pixel value to encoder
+    bl(ENCODE)
+    str(r0, [r3,0])    # store encoded value
