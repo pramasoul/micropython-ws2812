16519643e430f403fcca136637a962909fc93736
Tom Soulanille <soul@prama.com>
2015-10-13 21:23:13 -0700

    wspb.py: Pipe to ffmpeg to make movie

--- wspb.py
+++ wspb.py
@@ -1,6 +1,10 @@
 # playback for WS2812 recording
 
 #from collections import namedtuple
+import pdb
+import subprocess
+
+from PIL import Image, ImageDraw
 
 from pyb import _little_endian_int
 
@@ -27,7 +31,6 @@ class SPIRecording:
 
 
 class WS2812Recording(SPIRecording):
-
     def __next__(self):
         ts, data = SPIRecording.__next__(self)
         n = len(data) // (3*4)
@@ -37,11 +40,84 @@ class WS2812Recording(SPIRecording):
         return ts, colors
 
 
+class MovieFrames:
+    def __init__(self, fps=30, start=0, stop=None, dimensions=(512,512),
+                 dot_radius=4):
+        self.fps = fps
+        self.start = start
+        self.stop = stop
+        self.dimensions = dimensions
+        self.dot_radius = dot_radius
+        xdim = dimensions[0]
+        ydim = dimensions[1]
+        dx = 32
+        dy = 32
+        self.positions = [((dx*i + dx//2)%xdim, (dx*i + dx//2)//xdim * dy + dy//2) \
+                          for i in range(64)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        rv = Image.new('RGB', self.dimensions)
+        d = ImageDraw.Draw(rv)
+        r = self.dot_radius
+        t = self.leds()
+        if t is None:
+            raise StopIteration
+        #print(t)
+        positions, (ts, colors) = t
+        for pos, color in zip(positions, colors):
+            x, y = pos
+            d.ellipse((x-r, y-r, x+r, y+r), color)
+        return rv
+
+    def leds(self):
+        try:
+            return self.positions, next(self.recording)
+        except StopIteration:
+            return None
+
+
+class MovieMaker:
+    def __init__(self, frames):
+        self.frames = frames
+        
+    def make(self):
+        command = [ 'ffmpeg',
+                    '-y', # (optional) overwrite output file if it exists
+                    '-f', 'rawvideo',
+                    '-vcodec','rawvideo',
+                    '-s', '512x512', # size of one frame
+                    '-pix_fmt', 'rgb24',
+                    '-r', '10', # frames per second
+                    '-i', '-', # The imput comes from a pipe
+                    '-an', # Tells FFMPEG not to expect any audio
+                    '-vcodec', 'libx264',
+                    't.mov' ]
+        encoder = subprocess.Popen(command,
+                                   stdin=subprocess.PIPE)
+                                #stderr=subprocess.PIPE)
+        for frame in self.frames:
+            #print(frame)
+            #rlist, wlist, xlist = select.select([pipe.proc.stderr
+            encoder.stdin.write(frame.tobytes())
+        encoder.stdin.close()
+        
+
 def main(argv):
+    mf = MovieFrames()
     with open(argv[1], 'rb') as inf:
         spi_rec = WS2812Recording(inf)
-        for ts, colors in spi_rec:
-            print(ts, colors)
+        mf.recording = spi_rec
+        print(mf.positions)
+        #for i, frame in enumerate(mf):
+        #    print(i, end=' ')
+        #    frame.save('frame{:06}.png'.format(i))
+            #if i == 100:
+            #    break
+        mm = MovieMaker(mf)
+        mm.make()
     
 
 if __name__ == '__main__':
